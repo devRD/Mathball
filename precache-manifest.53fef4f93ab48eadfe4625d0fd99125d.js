12f94f52521bc3fab9e86765bf60d0f9916f289e
pbiswas101 <pbiswas101b@gmail.com>
2019-02-10 20:00:41 +0530

    Updates

---
+++ precache-manifest.53fef4f93ab48eadfe4625d0fd99125d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4730e0134a85c8278ad2311fa252f657",
+    "url": "/Mathball/static/media/logo.4730e013.png"
+  },
+  {
+    "revision": "0895d304447c392bb890",
+    "url": "/Mathball/static/js/runtime~main.0895d304.js"
+  },
+  {
+    "revision": "37c0d005b2d0dfc19779",
+    "url": "/Mathball/static/js/main.37c0d005.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/Mathball/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "37c0d005b2d0dfc19779",
+    "url": "/Mathball/static/css/main.0b2b3b84.chunk.css"
+  },
+  {
+    "revision": "cefccca8881ebfb107367b76356ccb06",
+    "url": "/Mathball/index.html"
+  }
+];
